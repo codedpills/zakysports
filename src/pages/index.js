126c5b9c207beb6a0f0b44763ffb6e68008de55d
zak <codedpills@gmail.com>
2021-01-29 14:59:34 +0000

    render footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Footer from "../components/footer/Footer"
 import Header from "../components/header/Header"
 
 const IndexPage = () => (
   <>
     <Header />
     <h1>Hello, welcome to ZakySports</h1>
+    <Footer />
   </>
 )
 
