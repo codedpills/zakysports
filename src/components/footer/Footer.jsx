6e1af788a4156f7eb06003d480942782d9e58114
zak <codedpills@gmail.com>
2021-01-29 14:59:25 +0000

    create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+                &copy; ZakySports 2021. All rights reserved.
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
