8cf6b4ae88c384ecf1f2ffba959b656f7f83016d
zak <codedpills@gmail.com>
2021-01-29 15:30:20 +0000

    render layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-import Footer from "../components/footer/Footer"
-import Header from "../components/header/Header"
+import Layout from "../components/layout"
 
 const IndexPage = () => (
   <>
-    <Header />
-    <h1>Hello, welcome to ZakySports</h1>
-    <Footer />
+    <Layout>
+      <h1>Hello, welcome to ZakySports</h1>
+    </Layout>
   </>
 )
 
--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 const News = () => {
     return (
-        <div>
+        <Layout>
             <h1>Welcome to the News page</h1>
-        </div>
+        </Layout>
     );
 }
 
