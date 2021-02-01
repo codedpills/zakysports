f82940e22f4f7dfbb1e3c92bc8e70c21652cac42
zak <codedpills@gmail.com>
2021-02-01 22:49:12 +0000

    apply global styles using styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import Footer from './footer/Footer';
-import Header from './header/Header';
+import React from "react"
+import { createGlobalStyle } from "styled-components"
+import Footer from "./footer/Footer"
+import Header from "./header/Header"
 
-const Layout = ({children}) => {
-    return (
-        <>
-        <Header />
-            {children}
-        <Footer />    
-        </>
-    );
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+    }
+`
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
-export default Layout;
+export default Layout
