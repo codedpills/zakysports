a6aa4e98a7d80479a68f9bec3cd8d3c09ceca387
zak <codedpills@gmail.com>
2021-02-01 22:25:10 +0000

    style footer using styled components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react"
+import styled from "styled-components"
+
+const StyledFooter = styled.div`
+  background-color: black;
+  color: white;
+  padding: 0.5rem;
+  margin-top: 2rem;
+`
+
+const StyledParagraph = styled.p`
+  text-align: center;
+`
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <p>
-                &copy; ZakySports 2021. All rights reserved.
-            </p>
-        </div>
-    );
+  return (
+    <StyledFooter className="footer">
+      <StyledParagraph>
+        &copy; ZakySports 2021. All rights reserved.
+      </StyledParagraph>
+    </StyledFooter>
+  )
 }
 
-export default Footer;
+export default Footer
